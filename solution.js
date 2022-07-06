c8ed05099e5e07fd44b3b665ea8795c99f40467b
Utsavi Patil <abc@gmail.com>
2022-07-06 16:32:29 +0530

    Logic added

--- solution.js
+++ solution.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+    let start = 0, end = 0;
 
+    while (start < end) {
+        if (str.charAt(start++) != str.charAt(end--)){
+            return false;
+        }
+    }
+
+    return true;
 }
 module.exports = palindrome
